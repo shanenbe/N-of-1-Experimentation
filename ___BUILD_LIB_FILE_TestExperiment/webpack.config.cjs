7afaf46d9757e8cd79d51811779683e1b35d20eb
shanenberg <stefan.hanenberg@uni-due.de>
2024-11-07 14:08:12 +0100

    Type system experiments with N-of-1 Experimentation in same project

---
+++ ___BUILD_LIB_FILE_TestExperiment/webpack.config.cjs
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './N-of-1-Experimentation/___BUILD_LIB_FILE_TestExperiment/API_PROVIDER_FOR_WEBPACK_LIB_GENERATION.ts',
+    devtool: "inline-source-map",
+    output: {
+        // path:'./',
+        path: path.resolve(__dirname, './'),
+        filename: './nof1experimentation.js',
+        // filename: './___BUILD_LIB_FILE_TestExperiment/experiment.js',
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+        fallback: { "crypto": false },
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+        ],
+    },
+
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: 'TestExperiment',
+    //         filename: 'index.html',
+    //         inject: true
+    //     })
+    // ]
+
+};
