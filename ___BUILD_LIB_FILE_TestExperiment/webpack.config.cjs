9f7cc3c44837e98360228a41de00da9ede272153
shanenberg <stefan.hanenberg@uni-due.de>
2025-01-10 14:12:55 +0100

    New nof1.js generation

--- ___BUILD_LIB_FILE_TestExperiment/webpack.config.cjs
+++ ___BUILD_LIB_FILE_TestExperiment/webpack.config.cjs
@@ -2,13 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './N-of-1-Experimentation/___BUILD_LIB_FILE_TestExperiment/API_PROVIDER_FOR_WEBPACK_LIB_GENERATION.ts',
+    entry: './___BUILD_LIB_FILE_TestExperiment/API_PROVIDER_FOR_WEBPACK_LIB_GENERATION.ts',
+    // entry: './___BUILD_LIB_FILE_TestExperiment/API_PROVIDER_FOR_WEBPACK_LIB_GENERATION.ts',
     devtool: "inline-source-map",
     output: {
         // path:'./',
         path: path.resolve(__dirname, './'),
         filename: './nof1experimentation.js',
-        // filename: './___BUILD_LIB_FILE_TestExperiment/experiment_configuration.js',
+        // filename: './___BUILD_LIB_FILE_TestExperiment/experiment.js',
     },
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ],
