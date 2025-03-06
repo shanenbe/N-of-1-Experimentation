74710314d9472f12430b24506148990f84e9e903
shanenberg <stefan.hanenberg@uni-due.de>
2025-03-06 12:26:50 +0100

    Init SQL experiment

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,14 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './___BUILD_CURRENT_EXPERIMENT/typescript/experiment_configuration2.ts',
+    entry: './___BUILD_CURRENT_EXPERIMENT/typescript/experiment_configuration.ts',
     // entry: './___BUILD_LIB_FILE_TestExperiment/API_PROVIDER_FOR_WEBPACK_LIB_GENERATION.ts',
     devtool: "inline-source-map",
     output: {
         // path:'./',
         path: path.resolve(__dirname, './'),
-        filename: './___BUILD_CURRENT_EXPERIMENT/experiment_configuration2.js',
-        // filename: './___BUILD_LIB_FILE_TestExperiment/experiment_configuration2.js',
+        filename: './___BUILD_CURRENT_EXPERIMENT/experiment_configuration.js',
+        // filename: './___BUILD_LIB_FILE_TestExperiment/experiment_configuration.js',
     },
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ],
